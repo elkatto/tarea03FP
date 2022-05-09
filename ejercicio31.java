c0329d9e5e1f916c7c37028d829c129c9087083a
elkatto <103528886+elkatto@users.noreply.github.com>
2022-05-09 16:28:19 -0500

    Add files via upload

---
+++ ejercicio31.java
@@ -0,0 +1,24 @@
+package TAREA3;
+
+import java.util.Scanner;
+
+public class ejercicio31 {
+    static Scanner lt = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // Declarar Variables
+        int edad;
+        String mensaje;
+        // Datos de entrada
+        System.out.println("Ingrese la edad:");
+        edad = lt.nextInt();
+        // Proceso
+        if (edad >= 18) {
+            mensaje = "Puede votar porque su edad es " + edad;
+        } else {
+            mensaje = "No puede votar porque su edad es " + edad;
+        }
+        // Datos de Salida
+        System.out.println(mensaje);
+    }
+}
